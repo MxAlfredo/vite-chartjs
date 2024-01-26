c203bf7dd56f11ea2e9ba7b36fa685d7b5f1b2b1
Jose Alfredo Escobar <alfredo.escobar@vincoorbis.com>
2024-01-26 10:11:43 -0600

    Correccion estilos

--- src/components/WeightedDailyUnavailability.vue
+++ src/components/WeightedDailyUnavailability.vue
@@ -194,7 +194,6 @@ watch([typeDisponibilidad, numberOfDecimals], () => {
 
 .chart-legend {
   font-size: 0.7rem;
-  font-weight: bold;
   text-align: left;
   margin-bottom: 10px;
   display: flex;
